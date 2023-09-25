0fe4a8f4738871db1349609f2d8d0822cb1ffd79
Shubham <shubhamlata2002@gmail.com>
2023-09-25 21:46:22 +0530

    ecommerce website using react

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 // import logo from './logo.svg';
-import './App.css';
-import CarOusel from './Components/CarOusel';
-import NavBar from './Components/NavBar';
-import ImgCards from './Components/ImgCards';
-import Footer from './Components/Footer';
-
-
+import "./App.css";
+import CarOusel from "./Components/CarOusel";
+import NavBar from "./Components/NavBar";
+import ImgCards from "./Components/ImgCards";
+import Footer from "./Components/Footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Contact from "./Components/Contact";
 
 function App() {
-  
   return (
-    <div >
-     <NavBar/>
-     <CarOusel/>
-     <ImgCards/>
-     <Footer/>
+    <div>
+      <Router>
+        <NavBar />
+        <CarOusel />
+        <ImgCards />
+
+        <Routes>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,24 +1,43 @@
-import React from 'react'
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Container from 'react-bootstrap/Container';
-const NavBar = () => { 
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Container from "react-bootstrap/Container";
+import { Link } from "react-router-dom";
+
+
+
+const NavBar = () => {
   return (
-    <div >
-       <Navbar  expand="lg"  style={{boxShadow: " 0 4px 8px 0 rgba(0,0,0,0.2)" ,color:'black'}} >
+    <Navbar
+      expand="lg"
+      style={{ boxShadow: " 0 4px 8px 0 rgba(0,0,0,0.2)", color: "black" }}
+    >
       <Container>
-        <Navbar.Brand href="#home" style={{color:'black', border:"2px solid orange", backgroundColor:'orange', borderRadius:"10px"}}>Realme Store</Navbar.Brand>
+        <Navbar.Brand
+          as={Link}
+          to="/"
+          style={{
+            color: "black",
+            border: "2px solid orange",
+            backgroundColor: "orange",
+            borderRadius: "10px",
+          }}
+        >
+          Realme Store
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+
             <NavDropdown title="Products" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#phones">Phones</NavDropdown.Item>
-              <NavDropdown.Item href="#accessories">Accessories</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/phones">Phones</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/accessories">Accessories</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
           </Nav>
           <Nav>
             <Nav.Link href="#cart">Cart</Nav.Link>
@@ -26,9 +45,9 @@ const NavBar = () => {
           </Nav>
         </Navbar.Collapse>
       </Container>
+     
     </Navbar>
-    </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
